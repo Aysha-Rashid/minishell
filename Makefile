654787989866aaf6cf4e35754341e6c255fb646d
Aysha Al Rashid <ayal-ras@lab2r1s14.42abudhabi.ae>
2024-01-28 14:05:19 +0400

    makefile and colors

--- Makefile
+++ Makefile
@@ -1,18 +1,29 @@
 NAME = minishell
 
-SRC =
+SRC = main.c\
 
-OBJ =
+OBJ = $(SRC:.c=.o)
 
 CC = cc
 
 CFLAGS = -Wall -Wextra -Werror
+# 033 is a esc in octal ascii, 0 sets all the text attributes to 0 and 32 is green
 
+COLOUR_GREEN=\033[0;32m
+COLOUR_END=\033[0m
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
+	@ echo "$(COLOUR_GREEN)compiled $(words $(OBJ)) files $(COLOUR_END)"
 all: $(NAME)
 
 clean:
-	rm -rf $(OBJ)
+	@ rm -rf $(OBJ)
+	@ echo "$(COLOUR_GREEN)Cleaned all 'o' files successfully$(COLOUR_END)"
 
 fclean: clean
+	@ rm -rf $(NAME)
+	@ echo "$(COLOUR_GREEN)Cleaned Program name successfully$(COLOUR_END)"
+
 
 re: fclean $(NAME)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ayal-ras <ayal-ras@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 12:16:37 by ayal-ras          #+#    #+#             */
-/*   Updated: 2024/01/28 12:26:24 by ayal-ras         ###   ########.fr       */
+/*   Updated: 2024/01/28 13:42:39 by ayal-ras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	
+	(void)env;
+	if (!argv || argc == 1)
+	{
+		printf("what? you don't want to work?\n");
+		exit (0);
+	}
+	else
+	{
+		printf("let's goooooo!\n");
+	}
 }
